b263951112ed5d49a620167fd7a68c03b716b797
Alexis Vidal <alexis.vidal@outlook.com>
2021-11-06 12:22:54 -0500

    Initial project

---
+++ Controllers/CacheController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ruleta.Redis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IDatabase _database;
+        public CacheController(IDatabase database)
+        {
+            _database = database;
+        }
+
+        // GET api/<CacheController>/5
+        [HttpGet]
+        public string Get([FromQuery]string key)
+        {
+            return _database.StringGet(key);
+        }
+
+        // POST api/<CacheController>
+        [HttpPost]
+        public void Post([FromBody] KeyValuePair<string, string> keyValue)
+        {
+            _database.StringSet(keyValue.Key, keyValue.Value);
+        }
+
+       
+    }
+}
